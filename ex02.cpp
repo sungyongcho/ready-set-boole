b3c45991c6a118a876c042ed298f28d6da65533a
Sungyong Cho <here@sungyongcho.com>
2023-06-29 17:48:50 +0200

    feat: ex02 done

--- ex02.cpp
+++ ex02.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+// https://ko.wikipedia.org/wiki/%EA%B7%B8%EB%A0%88%EC%9D%B4_%EB%B6%80%ED%98%B8
+// https://www.youtube.com/watch?v=5iCzSUU_ffw
+
 int gray_code(int n)
 {
-  (void) n;
-  return 1;
+  return n ^ (n >>1);
 }
 
 int main(){
-  std::cout << gray_code(1) << std::endl;
+  for (int i =0; i < 9; i++)
+    std::cout << gray_code(i) << std::endl;
 }
