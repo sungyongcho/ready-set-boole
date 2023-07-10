904c716c30d3e24ceafa225a72a8ad180c9474b8
Sungyong Cho <here@sungyongcho.com>
2023-07-10 04:58:30 +0200

    fix: ex08 showSubsetes wrong implementation

--- ex08.cpp
+++ ex08.cpp
@@ -6,29 +6,25 @@
 void showSubsets(const std::vector<std::vector<int> >& subsets) {
   std::cout << "Subsets:\n";
 
-  // Add an empty subset at the beginning
-  std::cout << "{ }\n";
-
   for (std::size_t i = 0; i < subsets.size(); i++) {
     const std::vector<int>& subset = subsets[i];
+    std::cout << "{ ";
     for (std::size_t j = 0; j < subset.size(); j++) {
-      std::cout << "{ ";
-      for (std::size_t k = 0; k <= j; k++) {
-        std::cout << subset[k];
-        if (k < j) {
-          std::cout << " ";
-        }
+      std::cout << subset[j];
+      if (j < subset.size() - 1) {
+        std::cout << " ";
       }
-      std::cout << " }\n";
     }
+    std::cout << " }\n";
   }
 }
 
 int main() {
+
   std::vector<int> set;
-  set.push_back(0);
+  // set.push_back(0);
   set.push_back(1);
-  // set.push_back(2);
-  // set.push_back(3);
+  set.push_back(2);
+  set.push_back(3);
   showSubsets(powerset(set));
 }
